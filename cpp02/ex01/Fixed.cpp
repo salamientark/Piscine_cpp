93c28f81bdd1c79142d8d02da30253ee0993648f
Jay Moker <dadamien78@gmail.com>
2024-07-28 19:37:03 +0200

    added int negative fixed point number

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -6,12 +6,13 @@
 /*   By: madlab <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 15:04:42 by madlab            #+#    #+#             */
-/*   Updated: 2024/07/24 11:43:53 by dbaladro         ###   ########.fr       */
+/*   Updated: 2024/07/28 19:27:58 by madlab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include <cmath>
+#include <ios>
 #include <iostream>
 #include <ostream>
 #include <cstdlib>
@@ -21,14 +22,24 @@ Fixed::Fixed( void ) : _nbr(0)
 	std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed( const int nbr ) : _nbr(nbr << 8)
+Fixed::Fixed( const int nbr )
 {
 	std::cout << "Int constructor was called" << std::endl;
+	std::cout << std::hex << nbr  << std::endl;
+	if ((nbr & 0x80000000) == 0) // Positive nbr
+		this->_nbr = ((nbr << 8) & 0x7FFFFFFF);
+	else
+		this->_nbr = ((nbr << 8) | 0x8000000);
+	std::cout << std::hex << this->_nbr << std::endl;
+	return ;
 }
 
 Fixed::Fixed( const float nbr )
 {
+	std::cout << "Float constructor called" << std::endl;
 	this->_nbr = (round(nbr * (1 << Fixed::_fractBitNbr)));
+
+	return ;
 }
 
 Fixed::Fixed( Fixed const & rhs )
@@ -70,24 +81,26 @@ void	Fixed::setRawBits( int const raw )
 // Public Memeber functions
 float	Fixed::toFloat( void ) const
 {
-	float	result;
+	char	sign = (this->_nbr & 0x80000000);
+	int		nbr_cp = ((this->_nbr & 0x80000000) != 0 ? ((this->_nbr ^ 0xFFFFFFFF) >> 8) + 1 : this->_nbr >> 8);
+	float	result = (float)nbr_cp;
 	int		mask = 128;
 	float	ref = 0.5f;
 
-	result = (float)(this->_nbr >> 8);
 	for (int i = 0; i < 8; i++)	
 	{
-		if (this->_nbr < 0)
-			result -= (ref * ((mask & this->_nbr) > 0));
-		else
-			result += (ref * ((mask & this->_nbr) > 0));
+		result += (ref * ((mask & this->_nbr) > 0));
 		ref /= 2;
 		mask = (mask >> 1);
 	}
-	return (result);
+	if (sign == 0)
+		return (result);
+	return (-result);
 }
 
 int		Fixed::toInt( void ) const
 {
-	return ((this->_nbr >> 8));
+	if ((this->_nbr & 0x80000000) == 0) // Positive
+		return ((this->_nbr >> 8));
+	return ((this->_nbr >> 8) | 0xFF000000);
 }
--- cpp02/ex01/main.cpp
+++ cpp02/ex01/main.cpp
@@ -1,4 +1,5 @@
 #include "Fixed.hpp"
+#include <ios>
 #include <iostream>
 int main( void ) {
 
@@ -9,6 +10,8 @@ int main( void ) {
 
 	a = Fixed( 1234.4321f );
 
+	// std::cout << std::fixed;
+	std::cout << std::dec;
 	std::cout << "a is " << a << std::endl;
 	std::cout << "b is " << b << std::endl;
 	std::cout << "c is " << c << std::endl;
@@ -19,11 +22,81 @@ int main( void ) {
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 
-	Fixed	h(2147483647);
-	std::cout << "h = " << h << std::endl;
+	// Custom
+	Fixed	p(1.0154524365f);
+	Fixed	o(45236.389678921658f);
+	Fixed	i(0.99759999f);
+	Fixed	u(-1.0f);
+	Fixed	w(-0.5666666f);
+	Fixed	y(-0.99999999f);
+	Fixed	e(-1.99999999f);
+	Fixed	t(-9786410.54981734619843f);
 
-	Fixed	j(-2147483647);
+	std::cout << std::dec;
+	std::cout << "p is " << p << std::endl;
+	std::cout << "p is " << p.toInt() << " as integer" << std::endl;
+	std::cout << "p is " << std::hex << p.toInt() << " as integer" << std::endl;
+
+	std::cout << std::dec;
+	std::cout << "o is " << o << std::endl;
+	std::cout << "o is " << o.toInt() << " as integer" << std::endl;
+	std::cout << "o is " << std::hex << o.toInt() << " as integer" << std::endl;
+
+	std::cout << std::dec;
+	std::cout << "i is " << i << std::endl;
+	std::cout << "i is " << i.toInt() << " as integer" << std::endl;
+	std::cout << "i is " << std::hex << i.toInt() << " as integer" << std::endl;
+
+	std::cout << std::dec;
+	std::cout << "u is " << u << std::endl;
+	std::cout << "u is " << u.toInt() << " as integer" << std::endl;
+	std::cout << "u is " << std::hex << u.toInt() << " as integer" << std::endl;
+
+	std::cout << std::dec;
+	std::cout << "w is " << w << std::endl;
+	std::cout << "w is " << w.toInt() << " as integer" << std::endl;
+	std::cout << "w is " << std::hex << w.toInt() << " as integer" << std::endl;
+
+	std::cout << std::dec;
+	std::cout << "y is " << y << std::endl;
+	std::cout << "y is " << y.toInt() << " as integer" << std::endl;
+	std::cout << "y is " << std::hex << y.toInt() << " as integer" << std::endl;
+
+	std::cout << std::dec;
+	std::cout << "e is " << e << std::endl;
+	std::cout << "e is " << e.toInt() << " as integer" << std::endl;
+	std::cout << "e is " << std::hex << e.toInt() << " as integer" << std::endl;
+
+	std::cout << std::dec;
+	std::cout << "t is " << t << std::endl;
+	std::cout << "t is " << t.toInt() << " as integer" << std::endl;
+	std::cout << "t is " << std::hex << t.toInt() << " as integer" << std::endl;
+
+	Fixed	h(2147483646);
+	Fixed	j(2147483647);
+	Fixed	k(-2147483646);
+	Fixed	l(-2147483647);
+
+	std::cout << std::fixed;
+	std::cout << std::dec;
+	std::cout << "h is " << h << std::endl;
+	std::cout << "h is " << h.toInt() << " as integer" << std::endl;
+	std::cout << "h is " << std::hex << h.toInt() << " as integer" << std::endl;
+
+	std::cout << std::dec;
 	std::cout << "j = " << j << std::endl;
+	std::cout << "j is " << j.toInt() << " as integer" << std::endl;
+	std::cout << "j is " << std::hex << j.toInt() << " as integer" << std::endl;
+
+	std::cout << std::dec;
+	std::cout << "k is " << k << std::endl;
+	std::cout << "k is " << k.toInt() << " as integer" << std::endl;
+	std::cout << "k is " << std::hex << k.toInt() << " as integer" << std::endl;
+
+	std::cout << std::dec;
+	std::cout << "l = " << l << std::endl;
+	std::cout << "l is " << l.toInt() << " as integer" << std::endl;
+	std::cout << "l is " << std::hex << l.toInt() << " as integer" << std::endl;
 
 	return 0;
 }
