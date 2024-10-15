3c33319dd721696bcdaa9f48c196c807e7e56a50
Jay Moker <dadamien78@gmail.com>
2024-10-15 09:58:19 +0200

    Merge sort V1

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: dbaladro <dbaladro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 11:40:49 by dbaladro          #+#    #+#             */
-/*   Updated: 2024/10/11 22:58:47 by dbaladro         ###   ########.fr       */
+/*   Updated: 2024/10/15 09:56:53 by dbaladro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,16 +34,27 @@ void	sort(std::vector<int>& v, int depth) {
 
 	std::vector<int>::iterator	end = ((v.size() / step) % 2 ? v.begin() + (v.size() / step) * step - 1 : v.end() - 1);
 
-	std::cout << "In sort" << std::endl;
+	// Perform swap
 	for (std::vector<int>::iterator it = v.begin() + step - 1; it < end && it != v.end(); it += step) {
 		if (it + step != v.end() && *(it + step) < *it)
-			std::swap_ranges(it + 1 - step, it + 1, it + step);
-		else
-			it += step;
-		std::cout << "*it : " << *it << std::endl;
+			std::swap_ranges(it + 1 - step, it + 1, it + 1);
+		// else
+		// 	it += step;
 		it += step;
 	}
-	std::cout << "End sort" << std::endl;
+
+	// PRINT STATUS FOR TESTING PURPOSE
+	std::cout << "Sorting depth = " << depth << " : ";
+	for (std::vector<int>::const_iterator it = v.begin(); it != v.end(); ++it) {
+		std::cout << *it;
+		if (it + 1 != v.end())
+			std::cout << " ";
+	}
+	std::cout << std::endl;
+
+	sort(v, depth + 1); // Perform recursion
+	
+	// Perform insertion part
 }
 
 /* ************************************************************************** */
